9b7f92450992c062e41a6e01c09cb67853804a02
Filippo <filippo@web3.foundation>
2026-01-27 13:54:56 +0100

    added test overrides Antiers tool

--- script_antiers.R
+++ script_antiers.R
@@ -27,13 +27,13 @@ for (j in 1:length(accounts)){
 
   # Fetch data from Subscan ----
 
-  scanner_data <- fetch_scanner_data(accounts[j], chain = "Kusama")
+  scanner_data <- fetch_scanner_data(accounts[j], chain = "Polkadot")
 
   # Compile simulations ----
 
-  sim_data <- jsonlite::fromJSON("validators_prediction_12814080.json")
+  sim_data <- jsonlite::fromJSON("validators_prediction.json")
 
-  summary_data <- summarize_sim_data_antiers(sim_data = sim_data, account = accounts[j], chain = "Kusama")
+  summary_data <- summarize_sim_data_antiers(sim_data = sim_data, account = accounts[j], chain = "Polkadot")
 
   # Tests ----
 
@@ -62,3 +62,41 @@ for (j in 1:length(accounts)){
 }
 
 do.call(rbind, big_data)
+
+
+# Manual override ----
+
+candidates <- c(
+  "14CRo92REj3aXfUeonVSti1VEHgxhWbtKY9hwxvD5T3BBXkK",
+  "12JZr1HgK8w6zsbBj6oAEVRkvisn8j3MrkXugqtvc4E8uwLo",
+  "13HtFCrxyz55KgkPWcnhHPwE8f8GmZrfXR3uC6jNrihGzmqz",
+  "16ARoGkkDSTmeu9tDvfBDksu4qURGz6s1HSvXzrwGnsjFtKg",
+  "14jgoaaLe7L12zhRnyACPQXhNtRNCXXygvHc3FgZFxmQkXBJ",
+  "168X6YCFoEDh8ZjxCcwzdjgZZhRjNHG6R45S7A3euxJcNXfN",
+  "133TTqBqmhHo9X5YWxYKB3JNJCuLdnLQZLbaZDHqprAKVxgD",
+  "141Qnt9LPnAfnurdY8RXcB8th5S5SNZbgk4ULpDJVdp5DSr2",
+  "144fdQQrXGwWWwATr9b3CL7RWQyxNRGMm2XJanVKGrNSoepc",
+  "13wVpSQPLxkFLSUsiAaChXKiTqHXfRGuevqGEo9ARobGqRZb",
+  "1AXSTNj1JhUpg83Rwq9ayBfHVnQmoXmpvYKDffLNdmNY9gm",
+  "161FEAs63HJqQnG2v4xgSpdfgPkDTqd3T8gzLzPaSnjPDaMZ",
+  "1o6KaLJCHyz2CoTkxBmqabvrPHyhcpnjn4jCbt16mPgEx8v",
+  "1bAKJKQg3bWCeNVzGuc1TzF2y2h4DJfWR92CWAoLVNn7nN8",
+  "1631N24odmeJguz7CvHMA6Bmqze4pG9RZwXV8DSAxci3Fwum",
+  "14TZSozkvCNsTsBYgZnZEwCLc7T6BzRw6pxTUjyL3FWqC7th",
+  "16kN6q7zAtiRGWFfa8sdC5vhnmE4nxdAeN8zX4RpR9E13tiV",
+  "14pRyAssutxsoZUmeyRqLCSUogihy8U9q4962N6mLZ8mLofx",
+  "16Xr1KqpLNcWidW7MoPtD8LSagE4QUnkLbi5aV5H72tSukvZ",
+  "13m2ttpCh1ho7rShHxsm8MtoJskzC6D4phaQGynHn3d8Q2MG",
+  "13Vuqok4VAnKghMtzxr6c2YwyG58HPZU9Xih2PhbDj8V9AoJ",
+  "1qQvg7wr5tHHog1eLrQ9PCQ4KAZN5DSWkVQvx9yzbizVeRJ",
+  "12bbPhujKmy6Ryd41mU1wwTjf3kZcwU3GfdCngY4VJiKBLrS",
+  "162uNfr8GeQiqmWnGnsp5vf1FkcBqF5rLiovEGCnn3f9CQFt",
+  "1puaLjmorA2b1CEn9eeqcT8z47X4Zd9eJi4TG6v6pYmbKYq",
+  "14xM5U9EhZ9FTTDmUWKPPvmRnQ3oF9NJANjnq8E5FsQDjnpn",
+  "165EH5A6sUMJDexTQrvsyw9yQtv1FxLe1F8NjL4wm2xQY6GD",
+  "14W4PGjfXFCPtx3oN2VRYn7e22n5mkzjAQzygmkNSkDvCSpV",
+  "14mM9FRDDtwSYicjNxSvMfQkap8o4m9zHq7hNW4JpbSL4PPU",
+  "12eXzTdsTTCYTEF8zJgv8bNbXA4M1k4ovSanu9t7dj24oAov"
+)
+
+sum(candidates %in% sim_data$results$account) / length(candidates)
